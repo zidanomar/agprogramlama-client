b129c2309deebb4abe0b4d73ffa5b82ac653a2f8
Zidan Omar Hamid <zidanomar.hamid@vivavis.com.tr>
2023-03-22 08:53:02 +0300

    main layout 🏒

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { MainLayout } from 'src/layout';
 import { MessagePage } from 'src/pages';
 import LoginPage from '../pages/Login/LoginPage';
 import PrivateRoute from './PrivateRoute';
@@ -6,11 +7,8 @@ import PrivateRoute from './PrivateRoute';
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <PrivateRoute>
-        <MessagePage />
-      </PrivateRoute>
-    ),
+    element: <MainLayout />,
+    children: [{ index: true, element: <MessagePage /> }],
   },
   {
     path: '/login',
